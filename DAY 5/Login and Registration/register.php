e0edc2d3ed169ffcd20518f76b251828129157c7
jismon33 <82600059+jismon33@users.noreply.github.com>
2021-08-25 20:59:30 +0530

    Add files via upload

---
+++ DAY 5/Login and Registration/register.php
@@ -0,0 +1,12 @@
+<?php
+$name=$_POST["name"];
+$mobile=$_POST["mobile"];
+$email=$_POST["email"];
+$address=$_POST["address"];
+$password=$_POST["password"];
+echo ("Your Name  is:     ".$name. "<br/>" );
+echo ("Your Mobile Number is:    ".$mobile. "<br/>" );
+echo ("Your Eamil id is:    ".$email. "<br/>" );
+echo ("Your Address is:    ".$address. "<br/>" );
+echo ("Your Password is:    ".$password. "<br/>" );
+?>
